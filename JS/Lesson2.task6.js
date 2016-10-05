06a0236ac953d5a5b6a4398a102434d0de7b9d0e
MaksKulikov <kulykov.maks@gmail.com>
2016-10-05 22:01:01 +0300

    data type conversion

---
+++ JS/Lesson2.task6.js
@@ -0,0 +1,11 @@
+/**
+ * Created by МАКС on 05.10.2016.
+ */
+var a = 12345;
+console.log(a);
+var str = String(a);
+console.log(str);
+var val = !!(str);
+console.log(val);
+var num = Number(val);
+console.log(num);

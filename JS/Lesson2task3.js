55b5d67b99e1674652e7cf5849de66410b6073f1
MaksKulikov <kulykov.maks@gmail.com>
2016-10-05 16:27:31 +0300

    created a variable assigned different values, led to the console

---
+++ JS/Lesson2task3.js
@@ -0,0 +1,14 @@
+/**
+ * Created by МАКС on 05.10.2016.
+ */
+var a;
+console.log(a);
+a = true;
+console.log(a);
+a = 77;
+console.log(a);
+a = "text";
+console.log(a);
+a = null;
+console.log(a);
+console.log(typeof a);

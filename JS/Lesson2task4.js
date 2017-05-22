1383fd7695ce30d29b0c84b197a997bd71454768
MaksKulykov <kulykov.maks@gmail.com>
2017-05-22 16:36:39 +0300

    deleted unused items

--- JS/Lesson2task4.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by МАКС on 05.10.2016.
- */
-"use strict";
-let a = "JavaScript";
-var b = "courses";
-var result;
-result = a + " " + b;
-console.log(result);
--- JS/test.js
+++
@@ -1,7 +0,0 @@
-for (i = 10; i >= 0; i--){
-    if( (i == 9) || (i == 7) || (i == 3 )){
-        continue;
-    }
-    num = i * 10;
-    document.write('Число: ' + '<b>' + num + '</b>' + '<br>');
-}
